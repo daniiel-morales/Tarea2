74536f4dff4f0990b2fb528687bac083fff1dbd4
danii_mor <daniiel.morales@outlook.com>
2020-02-14 23:23:56 -0600

    added a request to restaurant.js for requesting the food order.
    also json added the xmlreques package needed

--- client.js
+++ client.js
@@ -8,7 +8,7 @@
 // Modules required
 const http = require('http')
 const input = require('readline-sync')
- 
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 // Service parameters
 const host = '127.0.0.1'
@@ -17,20 +17,26 @@ const port = '14810'
 var order
 
 order = input.question('Bienvenido\n Que deseas ordenar?\n')
-console.log('Tu orden de: ' + order + ' esta en proceso...' )
+
+// send request order for restaurant
+var xhr = new XMLHttpRequest()
+xhr.open('POST', 'http://' + host + ':48101/' + order, true)
+xhr.send()
+
+console.log('Tu orden de: ' + order + ' esta en proceso...')
 
 // create a server order
 var clnt = http.createServer(function (req, res) {
-    // updates order status
-    if (req.url === '/1') {
-        console.log('Tu ' + order + ' va en camino!' )
-        res.end()
-        return
-    }
-    // ends order status
-    console.log('Orden de ' + order + ' entregada, vuelve pronto' )
+  // updates order status
+  if (req.url === '/1') {
+    console.log('Tu ' + order + ' va en camino!')
     res.end()
-    clnt.close()
+    return
+  }
+  // ends order status
+  console.log('Orden de ' + order + ' entregada, vuelve pronto')
+  res.end()
+  clnt.close()
 })
 
 // the server order listens on port
